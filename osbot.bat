37827dba3dc6749ac8929db0fbf4256fdd958a91
l33m4n <105036732+l33m4n@users.noreply.github.com>
2023-06-01 17:49:16 -0500

    Update osbot.bat
    Added dynamic username checking

--- osbot.bat
+++ osbot.bat
@@ -1,8 +1,11 @@
 @echo off
 
 :: code before loop ::
+
+SET dir="C:\Users\"%username%"\Documents"
+
 set /a rnd=(%RANDOM% %% (5005-5555))+5005
-cd/d "C:\Users\USERNAME\Documents"
+cd/d %dir%
 
 for /f delims^= %%i in ('
 dir/b/o-d "osbot*.jar"^|findstr/eil jar
@@ -14,4 +17,4 @@ for %%i in (cm,ss,ab,and,so,on
 )do 2>nul dir/b "%%~i-%_ver%.*"|findstr/eil jar
 
 :: code after get version ::
-java -Xmx3024M -Xms3024M -jar "%_ver%.jar" -debug %rnd%
+java -Xmx1024M -Xms1024M -jar "%_ver%.jar" -debug %rnd%
