d5c7ab6308092eb7c894aa46cc3e00aab90e5cbc
l33m4n <105036732+l33m4n@users.noreply.github.com>
2023-06-01 04:25:04 -0500

    Create script.bat
    EDIT LINES 21 AND 40

---
+++ script.bat
@@ -0,0 +1,55 @@
+echo off
+:begin
+echo Select a task:
+echo =============
+echo -
+echo 1) Open OSbot
+echo 2) Open Dreambot
+
+echo -
+set /p op=Type option:
+if "%op%"=="1" goto op1
+if "%op%"=="2" goto op2
+
+
+echo Please Pick an option:
+goto begin
+
+
+:op1
+echo you picked option 1
+cd/d "C:\Users\COMPUTER USERNAME\Documents"
+
+for /f delims^= %%i in ('
+dir/b/o-d "osbot*.jar"^|findstr/eil jar
+')do set "_ver=%%~ni" && goto %:next
+
+:next
+set "_ver=%_ver:*-=%"
+for %%i in (cm,ss,ab,and,so,on
+)do 2>nul dir/b "%%~i-%_ver%.*"|findstr/eil jar
+
+:: code after get version ::
+java -jar "%_ver%.jar"
+exit
+
+
+
+:op2
+echo you picked option 2
+cd/d "C:\Users\COMPUTER USERNAME\Documents"
+
+for /f delims^= %%i in ('
+dir/b/o-d "DBLauncher*.jar"^|findstr/eil jar
+')do set "_ver=%%~ni" && goto %:next
+
+:next
+set "_ver=%_ver:*-=%"
+for %%i in (cm,ss,ab,and,so,on
+)do 2>nul dir/b "%%~i-%_ver%.*"|findstr/eil jar
+
+:: code after get version ::
+java -jar "%_ver%.jar"
+
+:exit
+@exit
